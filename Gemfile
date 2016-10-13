444fc22cf7d6a8c45a670842a7840be9c5cf1d01
satoruk <satoru@catlet.com>
2016-10-14 00:43:54 +0900

    WIP

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 gem 'parallel'
 gem 'pry'
 gem 'pry-byebug'
+gem 'rainbow', require: ['rainbow/ext/string', 'rainbow']
--- sample_process01.rb
+++ sample_process01.rb
@@ -2,55 +2,62 @@ require 'bundler'
 Bundler.require
 
 class Sample
-  def worker_number
-    Thread.current[:parallel_worker_number]
+  COLORS = Rainbow::X11ColorNames::NAMES.keys.delete_if do |v|
+    [:darkslategray, :black, :white].include? v
   end
 
-  def worker_number=(worker_num)
-    puts "worker_number=(#{worker_num})"
-    Thread.current[:parallel_worker_number] = worker_num
+  def log(n, msg)
+    pid = Process.pid
+    tid = Thread.current.object_id
+    puts('pid:%s tid:%s [%s] %s' % [
+      sprintf('%-7d',  pid).background(:black).foreground(COLORS[pid % COLORS.size]),
+      sprintf('%-15d', tid).background(:black).foreground(COLORS[tid % COLORS.size]),
+      sprintf('%-6s',    n).background(:black).foreground(COLORS[n.object_id % COLORS.size]),
+      msg,
+    ])
   end
 
-  def run
-    pid = Process.fork do
-      self.worker_number = 100
-    end
-    puts pid
+
+  def child_eval(r, w)
+    log :child, "FD read:#{r.fileno} write:#{w.fileno}"
+    log :child, 'Wait the writable io object in a thread'
+    sleep 3
+    w.puts 'foo'
+    w.close
+  end
+
+  def parent_eval(r, w, pid)
+    log :parent, "child pid: #{pid}"
+    log :parent, "FD read:#{r.fileno} write:#{w.fileno}"
+    log :parent, "FD write:#{w.closed? ? 'close' : 'open'}"
+    sleep 5
+    log :parent, "FD write:#{w.closed? ? 'close' : 'open'}"
+    log :parent, r.gets
   end
 
   def pipe_run_with_process
+    log :parent, "call #{__method__}"
     r, w = IO.pipe
     pid = Process.fork do
-      puts "[child]pid: #{Process.pid}"
-      puts "[child] FD read:#{r.fileno} write:#{w.fileno}"
-      puts 'Wait the writable io object in a thread'
-      sleep 3
-      w.puts 'foo'
-      w.close
+      child_eval(r, w)
     end
-    puts "[parent]parent pid: #{Process.pid}"
-    puts "[parent]child pid: #{pid}"
-    puts "[parent] FD read:#{r.fileno} write:#{w.fileno}"
-    puts "[parent] FD read:#{w.closed? ? 'close' : 'open'}"
-    puts r.gets
+    parent_eval(r, w, pid)
   end
 
   def pipe_run_with_thread
+    log :parent, "call #{__method__}"
     r, w = IO.pipe
-    p [r, w] #=> [#<IO:fd 7>, #<IO:fd 8>]
     Thread.new do
-      puts 'Wait the writable io object in a thread'
-      sleep 3
-      w.puts 'foo'
-      w.close
+      child_eval(r, w)
     end
-    puts r.gets
+    parent_eval(r, w, Process.pid)
   end
 
 end
 
 sample = Sample.new
 sample.pipe_run_with_process
+#sample.pipe_run_with_thread
 #binding.pry
 
 puts 'finish'
